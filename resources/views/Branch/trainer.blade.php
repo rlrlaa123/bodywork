1e745f7a6cd76095e5b5308ca211ca3ba9103b67
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-08-14 21:44:23 +0900

    trainer

--- resources/views/Branch/trainer.blade.php
+++ resources/views/Branch/trainer.blade.php
@@ -127,7 +127,7 @@
         $('.trainer-btn').click(function () {
             var index = $(this).attr('class').slice(-1);
 
-            for (var i = 1; i <= {{ count($trainers) }}; i++) {
+            for (var i = 1; i <= {{ count($branches) }}; i++) {
                 $('.trainer-wrapper.' + i).css('display', 'none');
                 $('.trainer-btn')[i - 1].classList.remove('active');
             }
