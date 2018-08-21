a02ededcb76bea1ea367522fa940eec7b16beabb
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-08-21 23:11:39 +0900

    title

--- resources/views/layouts/app_dashboard.blade.php
+++ resources/views/layouts/app_dashboard.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <meta name="google-site-verification" content="cOAveFcpl0J7z3FJpsZNSRW4Qw9E4FWwADNrd3Eh7Qc" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,7 +9,6 @@
 
     <title>바디웍</title>
     <meta name="description" content=" 방이동 퍼스널트레이닝. 개인pt샵 바디웍입니다.">
-    <meta name="google-site-verification" content="cOAveFcpl0J7z3FJpsZNSRW4Qw9E4FWwADNrd3Eh7Qc" />
 
     @include('layouts.partial.bootstrap')
     @include('layouts.partial.jquery_js')
