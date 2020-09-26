34d6136d96455d0be85ea46249516e3d2ccdd60a
rlrlaa123 <kimdonghyun3366@gmail.com>
2020-09-26 17:20:39 +0900

    Branch: update naver map api

--- resources/views/Branch/branch.blade.php
+++ resources/views/Branch/branch.blade.php
@@ -9,7 +9,6 @@
             text-align: left;
             /*border: 1px solid #e3e3e3;*/
         }
-
         .branch-btn {
             border-right: 1px solid #e3e3e3;
             padding: 10px;
@@ -17,88 +16,71 @@
             color: white;
             width: 150px;
         }
-
         h3 {
             font-size: 2vw;
             margin: 0 50px;
         }
-
         hr {
             margin: 50px;
         }
-
         @media screen and (min-width: 1024px) and (max-width: 1440px) {
             .carousel {
                 margin: 0 50px;
                 height: 650px;
             }
-
             .carousel-inner {
                 height: 650px;
             }
-
             .phone-image {
                 width: 25px;
                 margin-right: 5px;
             }
         }
-
         @media screen and (min-width: 768px) and (max-width: 1024px) {
             .carousel {
                 margin: 0 50px;
                 height: 450px;
             }
-
             .carousel-inner {
                 height: 450px;
             }
-
             .phone-image {
                 width: 25px;
                 margin-right: 5px;
             }
         }
-
         @media screen and (min-width: 1441px) {
             .carousel {
                 margin: 0 50px;
                 height: 900px;
             }
-
             .carousel-inner {
                 height: 900px;
             }
-
             .phone-image {
                 width: 40px;
                 margin-right: 5px;
             }
         }
-
         @media screen and (max-width: 768px) and (orientation: landscape) {
             .carousel {
                 margin: 0 50px;
                 height: 450px;
             }
-
             .carousel-inner {
                 height: 450px;
             }
-
             .phone-image {
                 width: 25px;
                 margin-right: 5px;
             }
         }
-
         .carousel-indicators li {
             background-color: white;
         }
-
         .carousel-indicators .active {
             background-color: orange;
         }
-
         .branch-location {
             display: grid;
             display: -ms-grid;
@@ -108,13 +90,11 @@
             border-left: 2px solid orange;
             text-align: left;
         }
-
         .branch-location-text {
             text-align: left;
             padding: 0 20px;
             -ms-grid-column: 1;
         }
-
         .branch-location-map {
             padding: 20px;
             width: 100%;
@@ -122,69 +102,56 @@
             justify-content: center;
             -ms-grid-column: 2;
         }
-
         .branch-info-wrapper {
             text-align: left;
             margin: 0 50px;
             margin-bottom: 50px;
         }
-
         .branch-info {
             margin: 20px 0;
             border-bottom: 1px solid #e3e3e3;
         }
-
         .branch-info img {
             width: 25px;
         }
-
         .active {
             background-color: grey;
             border: none;
         }
-
         @media (orientation: portrait) {
             .carousel {
                 margin: 0 50px;
                 height: 200px;
             }
-
             .carousel-inner {
                 height: 200px;
             }
-
             .carousel-indicators {
                 bottom: 30px;
             }
-
             .carousel-item img {
                 height: 200px;
             }
-
             .branch-selector {
                 display: block;
                 grid-template-columns: repeat(1, 1fr);
                 width: 100%;
                 margin-bottom: 50px;
             }
-
             .branch-btn {
                 padding: 10px;
                 width: unset;
             }
-
             .branch-location {
                 display: block;
                 border: none;
             }
-
             .branch-location-text {
                 text-align: left;
                 padding: 0 20px;
                 border-left: 2px solid orange;
                 margin: 50px 0;
             }
-
             .branch-location-map {
                 padding: 20px;
                 width: 100%;
@@ -192,11 +159,9 @@
                 justify-content: center;
                 height: 200px;
             }
-
             .branch-info img {
                 width: 20px;
             }
-
             .phone-image {
                 width: 15px;
                 margin-right: 5px;
@@ -205,8 +170,7 @@
     </style>
 @endsection
 @section('content')
-    <script type="text/javascript"
-            src="https://openapi.map.naver.com/openapi/v3/maps.js?clientId=OmiueUFkWx50WHTaLjZl&submodules=geocoder"></script>
+    <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=dvfelyaqt6&submodules=geocoder"></script>
     <h3 style="margin: 50px 0; font-weight: bold;">바디웍 지점 소개</h3>
     <hr>
     <div style="display: flex; width: 100%;">
@@ -214,7 +178,7 @@
             @for($i = 1; $i <= count($branches); $i++)
                 @if($i == 1)
                     <a href="/branch/1/{{ $i }}">
-                    @php($pattern = '/\/branch\/1\/' . $i . '/')
+                        @php($pattern = '/\/branch\/1\/' . $i . '/')
                         <div class="branch-btn {{ $i }} {{ preg_match($pattern, $_SERVER['REQUEST_URI']) ? ' active' : '' }}">
                             {{ $i }}호점
                         </div>
@@ -310,35 +274,35 @@
 @endsection
 @section('script')
     <script>
-        var map = new naver.maps.Map('map');
-        var myaddress = '{{ $branch->location }}';// 도로명 주소나 지번 주소만 가능 (건물명 불가!!!!)
-        naver.maps.Service.geocode({address: myaddress}, function (status, response) {
-            if (status !== naver.maps.Service.Status.OK) {
-                return alert(myaddress + '의 검색 결과가 없거나 기타 네트워크 에러');
+        var map = new naver.maps.Map("map", {
+            center: new naver.maps.LatLng(37.3595316, 127.1052133),
+            zoom: 10,
+            mapTypeControl: true
+        });
+
+        var marker = new naver.maps.Marker({
+            position: new naver.maps.LatLng(37.3595316, 127.1052133),
+            map: map
+        });
+        naver.maps.Service.geocode({
+            query: '{{ $branch->location }}'
+        }, function(status, response) {
+            if (status === naver.maps.Service.Status.ERROR) {
+                return alert('Something Wrong!');
             }
-            var result = response.result;
-            // 검색 결과 갯수: result.total
-            // 첫번째 결과 결과 주소: result.items[0].address
-            // 첫번째 검색 결과 좌표: result.items[0].point.y, result.items[0].point.x
-            var myaddr = new naver.maps.Point(result.items[0].point.x, result.items[0].point.y);
-            map.setCenter(myaddr); // 검색된 좌표로 지도 이동
-            // 마커 표시
-            var marker = new naver.maps.Marker({
-                position: myaddr,
-                map: map
-            });
-            // 마커 클릭 이벤트 처리
-            naver.maps.Event.addListener(marker, "click", function (e) {
-                if (infowindow.getMap()) {
-                    infowindow.close();
-                } else {
-                    infowindow.open(map, marker);
-                }
-            });
-            // 마크 클릭시 인포윈도우 오픈
-            var infowindow = new naver.maps.InfoWindow({
-                content: '<h4> [네이버 개발자센터]</h4><a href="https://developers.naver.com" target="_blank"><img src="https://developers.naver.com/inc/devcenter/images/nd_img.png"></a>'
-            });
+
+            if (response.v2.meta.totalCount === 0) {
+                return alert('totalCount' + response.v2.meta.totalCount);
+            }
+
+            var htmlAddresses = [],
+                item = response.v2.addresses[0],
+                point = new naver.maps.Point(item.x, item.y);
+            if (item.roadAddress) {
+                htmlAddresses.push('<h6>' + item.roadAddress + '</h6>');
+            }
+            map.setCenter(point);
+            marker.setPosition({x: item.x, y: item.y})
         });
     </script>
 @endsection
